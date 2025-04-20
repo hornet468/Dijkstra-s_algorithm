bfc908ce16494aa03400dc5cd07ff2749159a246
hornet468 <tomhelington@gmail.com>
2025-04-20 08:52:24 +0000

    Basic graph

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,18 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        Graph graph = new Graph();
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
+        graph.addVertex("D");
 
+        graph.addEdge("A", "B");
+        graph.addEdge("A", "D");
+        graph.addEdge("B", "C");
+        graph.addEdge("B", "D");
+        graph.addEdge("C", "D");
 
+        graph.search("A");
     }
 }
